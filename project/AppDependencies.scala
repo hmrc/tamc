1db62fd1fb26f80bc8c272d0994eb4dee2405bd3
nik <ward.nickjames@gmail.com>
2022-07-11 14:03:07 +0100

    Update bootstrap-backend-play28

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
     ws,
     "uk.gov.hmrc"       %% "domain"                    % "8.1.0-play-28",
     "uk.gov.hmrc"       %% "emailaddress"              % "3.6.0",
-    "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % "5.18.0",
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % "6.3.0",
     "uk.gov.hmrc"       %% "tax-year"                  % "3.0.0"
   )
 
