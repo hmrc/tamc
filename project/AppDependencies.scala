11400f27c6ee7aa7f6373a891ee032960e0f41bc
Nick Ward <ward.nickjames@gmail.com>
2022-07-14 10:36:24 +0100

    [DDCNL-5936] Update dependencies (#62)
    * Update bootstrap-backend-play28
    
    * Update jsoup
    
    * Fix failing integration tests
    
    * Update mockito
    
    * Update to slightly higher flexmark version
    
    * Update sbt version
    
    * Update plugins

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,16 +8,16 @@ object AppDependencies {
     ws,
     "uk.gov.hmrc"       %% "domain"                    % "8.1.0-play-28",
     "uk.gov.hmrc"       %% "emailaddress"              % "3.6.0",
-    "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % "5.18.0",
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % "6.3.0",
     "uk.gov.hmrc"       %% "tax-year"                  % "3.0.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.jsoup"                % "jsoup"              % "1.14.3",
+    "org.jsoup"                % "jsoup"              % "1.15.2",
     "org.scalatestplus.play"  %% "scalatestplus-play" % "5.1.0",
-    "org.mockito"              % "mockito-core"       % "4.5.1",
+    "org.mockito"              % "mockito-core"       % "4.6.1",
     "com.github.tomakehurst"   % "wiremock-jre8"      % "2.27.2",
-    "com.vladsch.flexmark"     % "flexmark-all"       % "0.35.10"
+    "com.vladsch.flexmark"     % "flexmark-all"       % "0.36.8"
   ).map(_ % "test,it")
 
   private val silencerDependencies: Seq[ModuleID] = Seq(
