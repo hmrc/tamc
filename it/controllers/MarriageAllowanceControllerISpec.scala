82972d04763d1fcfdd565d1f99ec503621764393
Ian Godman <IanPGodman@users.noreply.github.com>
2022-10-18 09:37:49 +0100

    Reverted integration test to current main branch version.

--- it/controllers/MarriageAllowanceControllerISpec.scala
+++ it/controllers/MarriageAllowanceControllerISpec.scala
@@ -103,7 +103,7 @@ class MarriageAllowanceControllerISpec extends IntegrationSpec {
           val result = route(fakeApplication(), request)
           val expected = Json.toJson(RelationshipRecordStatusWrapper(status = ResponseStatus(status_code = errorCode)))
 
-          result.map(getStatus) shouldBe Some(OK)
+          result.map(getStatus) shouldBe Some(httpErrorCode.toInt)
           result.map(contentAsJson) shouldBe Some(expected)
         }
 
@@ -122,7 +122,7 @@ class MarriageAllowanceControllerISpec extends IntegrationSpec {
           val result = route(fakeApplication(), request)
           val expected = Json.toJson(RelationshipRecordStatusWrapper(status = ResponseStatus(status_code = errorCode)))
 
-          result.map(getStatus) shouldBe Some(OK)
+          result.map(getStatus) shouldBe Some(httpErrorCode.toInt)
           result.map(contentAsJson) shouldBe Some(expected)
         }
       }
