79b663905bfae8d1abc30ddd14b65d4ab324f133
Derek Gray <derek.gray@digital.hmrc.gov.uk>
2023-01-15 18:07:32 +0000

    Upgrade Libraries

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,15 +7,15 @@ object AppDependencies {
   val compile = Seq(
     ws,
     "uk.gov.hmrc"       %% "domain"                    % "8.1.0-play-28",
-    "uk.gov.hmrc"       %% "emailaddress"              % "3.6.0",
-    "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % "6.3.0",
+    "uk.gov.hmrc"       %% "emailaddress"              % "3.7.0",
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % "7.12.0",
     "uk.gov.hmrc"       %% "tax-year"                  % "3.0.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.jsoup"                % "jsoup"              % "1.15.2",
+    "org.jsoup"                % "jsoup"              % "1.15.3",
     "org.scalatestplus.play"  %% "scalatestplus-play" % "5.1.0",
-    "org.mockito"              % "mockito-core"       % "4.6.1",
+    "org.mockito"              % "mockito-core"       % "4.11.0",
     "com.github.tomakehurst"   % "wiremock-jre8"      % "2.27.2",
     "com.vladsch.flexmark"     % "flexmark-all"       % "0.36.8"
   ).map(_ % "test,it")
