4ee172ecad5f74b2e2941919d7b0b9940646eb53
AndrewBrown22 <45789050+andrewbrown22@users.noreply.github.com>
2024-08-15 13:21:43 +0100

    DDCNL-9308 fixed unit tests

--- test/controllers/auth/AuthActionSpec.scala
+++ test/controllers/auth/AuthActionSpec.scala
@@ -16,7 +16,6 @@
 
 package controllers.auth
 
-import controllers.auth.AuthConnector
 import org.mockito.ArgumentMatchers._
 import org.mockito.Mockito._
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
